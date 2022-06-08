f8d1dc61674484a0b40a773e6ba4192535ef4c8e
盲果汁 <1187916635@qq.com>
2022-06-08 15:15:47 +0800

    添加项目文件。

---
+++ Tempe-4show/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Tempe_4show
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.uiLine1 = new Sunny.UI.UILine();
+            this.uiLine2 = new Sunny.UI.UILine();
+            this.uiLine3 = new Sunny.UI.UILine();
+            this.uiLine4 = new Sunny.UI.UILine();
+            this.uiAvatar1 = new Sunny.UI.UIAvatar();
+            this.uiAvatar2 = new Sunny.UI.UIAvatar();
+            this.uiAvatar3 = new Sunny.UI.UIAvatar();
+            this.uiAvatar4 = new Sunny.UI.UIAvatar();
+            this.uiTextBox1 = new Sunny.UI.UIScrollingText();
+            this.uiTextBox3 = new Sunny.UI.UIScrollingText();
+            this.uiTextBox2 = new Sunny.UI.UIScrollingText();
+            this.uiTextBox4 = new Sunny.UI.UIScrollingText();
+            this.uiTextBox5 = new Sunny.UI.UIScrollingText();
+            this.uiTextBox7 = new Sunny.UI.UIScrollingText();
+            this.uiTextBox6 = new Sunny.UI.UIScrollingText();
+            this.uiTextBox8 = new Sunny.UI.UIScrollingText();
+            this.SuspendLayout();
+            // 
+            // uiLine1
+            // 
+            this.uiLine1.FillColor = System.Drawing.Color.DimGray;
+            this.uiLine1.Font = new System.Drawing.Font("华文彩云", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLine1.ForeColor = System.Drawing.Color.Lime;
+            this.uiLine1.LineSize = 3;
+            this.uiLine1.Location = new System.Drawing.Point(82, 0);
+            this.uiLine1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiLine1.Name = "uiLine1";
+            this.uiLine1.RadiusSides = Sunny.UI.UICornerRadiusSides.RightTop;
+            this.uiLine1.Size = new System.Drawing.Size(85, 29);
+            this.uiLine1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLine1.TabIndex = 2;
+            this.uiLine1.Text = "软件";
+            this.uiLine1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiLine1.Click += new System.EventHandler(this.uiLine1_Click);
+            // 
+            // uiLine2
+            // 
+            this.uiLine2.FillColor = System.Drawing.Color.Transparent;
+            this.uiLine2.Font = new System.Drawing.Font("华文彩云", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLine2.ForeColor = System.Drawing.Color.Yellow;
+            this.uiLine2.LineSize = 3;
+            this.uiLine2.Location = new System.Drawing.Point(175, 113);
+            this.uiLine2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiLine2.Name = "uiLine2";
+            this.uiLine2.Size = new System.Drawing.Size(85, 29);
+            this.uiLine2.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLine2.TabIndex = 4;
+            this.uiLine2.Text = "会议";
+            this.uiLine2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLine3
+            // 
+            this.uiLine3.FillColor = System.Drawing.Color.Transparent;
+            this.uiLine3.Font = new System.Drawing.Font("华文彩云", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLine3.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.uiLine3.LineSize = 3;
+            this.uiLine3.Location = new System.Drawing.Point(78, 113);
+            this.uiLine3.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiLine3.Name = "uiLine3";
+            this.uiLine3.Size = new System.Drawing.Size(85, 29);
+            this.uiLine3.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLine3.TabIndex = 5;
+            this.uiLine3.Text = "机械";
+            this.uiLine3.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLine4
+            // 
+            this.uiLine4.FillColor = System.Drawing.Color.Transparent;
+            this.uiLine4.Font = new System.Drawing.Font("华文彩云", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLine4.ForeColor = System.Drawing.Color.Violet;
+            this.uiLine4.LineSize = 3;
+            this.uiLine4.Location = new System.Drawing.Point(175, 0);
+            this.uiLine4.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiLine4.Name = "uiLine4";
+            this.uiLine4.Size = new System.Drawing.Size(85, 29);
+            this.uiLine4.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLine4.TabIndex = 6;
+            this.uiLine4.Text = "硬件";
+            this.uiLine4.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiAvatar1
+            // 
+            this.uiAvatar1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.uiAvatar1.Font = new System.Drawing.Font("华文彩云", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiAvatar1.ForeColor = System.Drawing.Color.Black;
+            this.uiAvatar1.Icon = Sunny.UI.UIAvatar.UIIcon.Text;
+            this.uiAvatar1.Location = new System.Drawing.Point(12, 35);
+            this.uiAvatar1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAvatar1.Name = "uiAvatar1";
+            this.uiAvatar1.Shape = Sunny.UI.UIShape.Square;
+            this.uiAvatar1.Size = new System.Drawing.Size(60, 31);
+            this.uiAvatar1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiAvatar1.TabIndex = 7;
+            this.uiAvatar1.Text = "温度";
+            this.uiAvatar1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiAvatar2
+            // 
+            this.uiAvatar2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.uiAvatar2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiAvatar2.ForeColor = System.Drawing.Color.Blue;
+            this.uiAvatar2.Icon = Sunny.UI.UIAvatar.UIIcon.Text;
+            this.uiAvatar2.Location = new System.Drawing.Point(12, 73);
+            this.uiAvatar2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAvatar2.Name = "uiAvatar2";
+            this.uiAvatar2.Shape = Sunny.UI.UIShape.Square;
+            this.uiAvatar2.Size = new System.Drawing.Size(60, 31);
+            this.uiAvatar2.Style = Sunny.UI.UIStyle.Custom;
+            this.uiAvatar2.TabIndex = 7;
+            this.uiAvatar2.Text = "湿度";
+            this.uiAvatar2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiAvatar3
+            // 
+            this.uiAvatar3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.uiAvatar3.Font = new System.Drawing.Font("华文彩云", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiAvatar3.ForeColor = System.Drawing.Color.Black;
+            this.uiAvatar3.Icon = Sunny.UI.UIAvatar.UIIcon.Text;
+            this.uiAvatar3.Location = new System.Drawing.Point(12, 147);
+            this.uiAvatar3.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAvatar3.Name = "uiAvatar3";
+            this.uiAvatar3.Shape = Sunny.UI.UIShape.Square;
+            this.uiAvatar3.Size = new System.Drawing.Size(60, 31);
+            this.uiAvatar3.Style = Sunny.UI.UIStyle.Custom;
+            this.uiAvatar3.TabIndex = 7;
+            this.uiAvatar3.Text = "温度";
+            this.uiAvatar3.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiAvatar4
+            // 
+            this.uiAvatar4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.uiAvatar4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiAvatar4.ForeColor = System.Drawing.Color.Blue;
+            this.uiAvatar4.Icon = Sunny.UI.UIAvatar.UIIcon.Text;
+            this.uiAvatar4.Location = new System.Drawing.Point(12, 184);
+            this.uiAvatar4.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAvatar4.Name = "uiAvatar4";
+            this.uiAvatar4.Shape = Sunny.UI.UIShape.Square;
+            this.uiAvatar4.Size = new System.Drawing.Size(60, 31);
+            this.uiAvatar4.Style = Sunny.UI.UIStyle.Custom;
+            this.uiAvatar4.TabIndex = 7;
+            this.uiAvatar4.Text = "湿度";
+            this.uiAvatar4.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTextBox1
+            // 
+            this.uiTextBox1.FillColor = System.Drawing.Color.White;
+            this.uiTextBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.uiTextBox1.Location = new System.Drawing.Point(82, 35);
+            this.uiTextBox1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox1.Name = "uiTextBox1";
+            this.uiTextBox1.Size = new System.Drawing.Size(85, 31);
+            this.uiTextBox1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiTextBox1.TabIndex = 8;
+            this.uiTextBox1.Text = "0.00";
+            this.uiTextBox1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTextBox3
+            // 
+            this.uiTextBox3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox3.ForeColor = System.Drawing.Color.Black;
+            this.uiTextBox3.Location = new System.Drawing.Point(175, 35);
+            this.uiTextBox3.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox3.Name = "uiTextBox3";
+            this.uiTextBox3.Size = new System.Drawing.Size(85, 31);
+            this.uiTextBox3.Style = Sunny.UI.UIStyle.Custom;
+            this.uiTextBox3.TabIndex = 9;
+            this.uiTextBox3.Text = "0.00";
+            this.uiTextBox3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox3.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTextBox2
+            // 
+            this.uiTextBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox2.ForeColor = System.Drawing.Color.Black;
+            this.uiTextBox2.Location = new System.Drawing.Point(82, 73);
+            this.uiTextBox2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox2.Name = "uiTextBox2";
+            this.uiTextBox2.Size = new System.Drawing.Size(85, 31);
+            this.uiTextBox2.Style = Sunny.UI.UIStyle.Custom;
+            this.uiTextBox2.TabIndex = 9;
+            this.uiTextBox2.Text = "0.00";
+            this.uiTextBox2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTextBox4
+            // 
+            this.uiTextBox4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox4.ForeColor = System.Drawing.Color.Black;
+            this.uiTextBox4.Location = new System.Drawing.Point(175, 73);
+            this.uiTextBox4.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox4.Name = "uiTextBox4";
+            this.uiTextBox4.Size = new System.Drawing.Size(85, 31);
+            this.uiTextBox4.Style = Sunny.UI.UIStyle.Custom;
+            this.uiTextBox4.TabIndex = 10;
+            this.uiTextBox4.Text = "0.00";
+            this.uiTextBox4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox4.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTextBox5
+            // 
+            this.uiTextBox5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox5.ForeColor = System.Drawing.Color.Black;
+            this.uiTextBox5.Location = new System.Drawing.Point(82, 147);
+            this.uiTextBox5.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox5.Name = "uiTextBox5";
+            this.uiTextBox5.Size = new System.Drawing.Size(85, 31);
+            this.uiTextBox5.Style = Sunny.UI.UIStyle.Custom;
+            this.uiTextBox5.TabIndex = 11;
+            this.uiTextBox5.Text = "0.00";
+            this.uiTextBox5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox5.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTextBox7
+            // 
+            this.uiTextBox7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox7.ForeColor = System.Drawing.Color.Black;
+            this.uiTextBox7.Location = new System.Drawing.Point(175, 147);
+            this.uiTextBox7.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox7.Name = "uiTextBox7";
+            this.uiTextBox7.Size = new System.Drawing.Size(85, 31);
+            this.uiTextBox7.Style = Sunny.UI.UIStyle.Custom;
+            this.uiTextBox7.TabIndex = 12;
+            this.uiTextBox7.Text = "0.00";
+            this.uiTextBox7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox7.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTextBox6
+            // 
+            this.uiTextBox6.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox6.ForeColor = System.Drawing.Color.Black;
+            this.uiTextBox6.Location = new System.Drawing.Point(82, 184);
+            this.uiTextBox6.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox6.Name = "uiTextBox6";
+            this.uiTextBox6.Size = new System.Drawing.Size(85, 31);
+            this.uiTextBox6.Style = Sunny.UI.UIStyle.Custom;
+            this.uiTextBox6.TabIndex = 13;
+            this.uiTextBox6.Text = "0.00";
+            this.uiTextBox6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox6.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTextBox8
+            // 
+            this.uiTextBox8.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox8.ForeColor = System.Drawing.Color.Black;
+            this.uiTextBox8.Location = new System.Drawing.Point(175, 184);
+            this.uiTextBox8.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTextBox8.Name = "uiTextBox8";
+            this.uiTextBox8.Size = new System.Drawing.Size(85, 31);
+            this.uiTextBox8.Style = Sunny.UI.UIStyle.Custom;
+            this.uiTextBox8.TabIndex = 14;
+            this.uiTextBox8.Text = "0.00";
+            this.uiTextBox8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox8.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(268, 223);
+            this.Controls.Add(this.uiTextBox8);
+            this.Controls.Add(this.uiTextBox6);
+            this.Controls.Add(this.uiTextBox7);
+            this.Controls.Add(this.uiTextBox5);
+            this.Controls.Add(this.uiTextBox4);
+            this.Controls.Add(this.uiTextBox2);
+            this.Controls.Add(this.uiTextBox3);
+            this.Controls.Add(this.uiTextBox1);
+            this.Controls.Add(this.uiAvatar4);
+            this.Controls.Add(this.uiAvatar3);
+            this.Controls.Add(this.uiAvatar2);
+            this.Controls.Add(this.uiAvatar1);
+            this.Controls.Add(this.uiLine4);
+            this.Controls.Add(this.uiLine3);
+            this.Controls.Add(this.uiLine2);
+            this.Controls.Add(this.uiLine1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(286, 270);
+            this.MinimumSize = new System.Drawing.Size(286, 270);
+            this.Name = "Form1";
+            this.Text = "温湿度";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Sunny.UI.UILine uiLine1;
+        private Sunny.UI.UILine uiLine2;
+        private Sunny.UI.UILine uiLine3;
+        private Sunny.UI.UILine uiLine4;
+        private Sunny.UI.UIAvatar uiAvatar1;
+        private Sunny.UI.UIAvatar uiAvatar2;
+        private Sunny.UI.UIAvatar uiAvatar3;
+        private Sunny.UI.UIAvatar uiAvatar4;
+        private Sunny.UI.UIScrollingText uiTextBox1;
+        private Sunny.UI.UIScrollingText uiTextBox3;
+        private Sunny.UI.UIScrollingText uiTextBox2;
+        private Sunny.UI.UIScrollingText uiTextBox4;
+        private Sunny.UI.UIScrollingText uiTextBox5;
+        private Sunny.UI.UIScrollingText uiTextBox7;
+        private Sunny.UI.UIScrollingText uiTextBox6;
+        private Sunny.UI.UIScrollingText uiTextBox8;
+    }
+}
+
